28e5d78cd7cfaf21705fc1cdee92c59fd8bf2a52
danielsmo <danielsmota07@gmail.com>
2022-10-02 18:29:29 -0300

    atualizado

--- src/main/java/com/example/demo/Candidato.java
+++ src/main/java/com/example/demo/Candidato.java
@@ -1,12 +1,13 @@
 package com.example.demo;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
 
 public class Candidato {
 
-    @JsonProperty("cand")
+    @JsonAlias("cand")
     private List<Dados> dados;
 
     public List<Dados> getDados() {
